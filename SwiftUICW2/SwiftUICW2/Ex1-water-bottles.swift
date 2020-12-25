88501d32fa574b3ba88a6e6e9a8b6156a48f20e4
Nasser Alsaqabi <alsokor4@gmail.com>
2020-12-25 20:50:53 +0300

    water bottles Done

--- SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
+++ SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
@@ -7,6 +7,8 @@
 import SwiftUI
 
 struct Exercise1: View {
+    @State var name =  ""
+    @State var num = 0
     var body: some View {
         ZStack {
             Image("charter")
@@ -20,6 +22,15 @@ struct Exercise1: View {
                     .fontWeight(.black)
                 
 //                ADD THE CODE HERE
+            
+                    TextField("أكتب اسمك هنا", text: $name) // don't foregt the $ for the var. to be a binding string
+                    
+                    Stepper("كم بطلا من الماء تريدأن تتعهد بأن تشرب؟", value: $num, in: 1...122)
+                Spacer()
+                    Text("أتعهد أنا , \(name) أن أشرب \(num) أكواباً من الماء يوميا والله على ما اقول شهيد")
+                        .font(.system(size: 35))
+
+                
                 
                 
                 Spacer()        
